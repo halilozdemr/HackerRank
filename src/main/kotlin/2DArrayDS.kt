40311cb0f12b1ccf570b3a1c6bce8c6abd7743c3
Halil İbrahim Özdemir <ab268520@adalet.gov.tr>
2021-05-22 12:51:14 +0300

    2D Array - DS Solution

---
+++ src/main/kotlin/2DArrayDS.kt
@@ -0,0 +1,32 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val arr = Array(6) { Array(6) { 0 } }
+
+    for (i in 0 until 6) {
+        arr[i] = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+    }
+
+    val result = hourglassSum(arr)
+
+    println(result)
+}
+
+fun hourglassSum(arr: Array<Array<Int>>): Int {
+    var result = -1000
+    for (i in 0..3) {
+        for (j in 0..3) {
+            val sum =
+                arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+
+            if (sum > result)
+                result = sum
+
+        }
+    }
+
+    return result
+}
+
